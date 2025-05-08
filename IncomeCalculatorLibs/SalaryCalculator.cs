13938600fe7b35ad6869e5f5fb99b507e0ed245b
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2025-05-08 11:34:03 +1000

    Add project files.

---
+++ IncomeCalculatorLibs/SalaryCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace IncomeCalculatorLibs
+{
+    public class SalaryCalculator
+    {
+        const int hoursInYear = 2080;
+        //Methods/Functions that do the salary calculations
+        public decimal GetAnnualSalary(decimal hourlyWage)
+        {
+            decimal annualSal = hourlyWage * hoursInYear;
+            return annualSal;
+        }
+
+        //2nd method
+    }
+}
---
+++ SalCalcTest25/SalaryCalculatorTestC.cs
@@ -0,0 +1,35 @@
+﻿using IncomeCalculatorLibs;
+
+namespace SalCalcTest25
+{
+    ///<summary>
+    ///Setup a Salary Calculator
+    ///To get hourly, divide annual salary by 2080
+    ///$100,006.40 / 2080 = $48.08 hr
+    ///To get annual, multiply hourly by 2080
+    ///$48.08 * 2080 = $100,006.40
+    ///</summary>
+    [TestClass]
+    public sealed class SalaryCalculatorTestC
+    {
+        /// <summary>
+        /// Test the Hourly Salary
+        /// </summary>
+        [TestMethod]
+        public void AnnualSalaryTest()
+        {
+            //Arange - Setup the data
+            SalaryCalculator calculator = new SalaryCalculator();
+            decimal wage = 48.08m;
+            decimal exAnnualSalary = 100006.40m;
+
+            //Act - Access the data
+            decimal actAnnualSalary = calculator.GetAnnualSalary(wage);
+
+            //Assert - Check the data
+            //Assert.AreEqual(expected vs actual)
+            Assert.AreEqual(exAnnualSalary, actAnnualSalary);
+
+        }
+    }
+}

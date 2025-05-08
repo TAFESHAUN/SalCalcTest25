6083dd3780fc56982f4a79c22850c729fe8695b9
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2025-05-08 12:02:01 +1000

    Updated with wage calc and test

--- IncomeCalculatorLibs/SalaryCalculator.cs
+++ IncomeCalculatorLibs/SalaryCalculator.cs
@@ -4,12 +4,8 @@
     {
         const int hoursInYear = 2080;
         //Methods/Functions that do the salary calculations
-        public decimal GetAnnualSalary(decimal hourlyWage)
-        {
-            decimal annualSal = hourlyWage * hoursInYear;
-            return annualSal;
-        }
-
-        //2nd method
+        public decimal GetAnnualSalary(decimal hourlyWage) => hourlyWage * hoursInYear;
+       //Method that gets the hourly wage
+       public decimal GetHourlyWage(decimal annualSal) => annualSal / hoursInYear;
     }
 }
--- SalCalcTest25/SalaryCalculatorTestC.cs
+++ SalCalcTest25/SalaryCalculatorTestC.cs
@@ -4,6 +4,7 @@ namespace SalCalcTest25
 {
     ///<summary>
     ///Setup a Salary Calculator
+    /// Use the AAA OF TESTING
     ///To get hourly, divide annual salary by 2080
     ///$100,006.40 / 2080 = $48.08 hr
     ///To get annual, multiply hourly by 2080
@@ -12,6 +13,12 @@ namespace SalCalcTest25
     [TestClass]
     public sealed class SalaryCalculatorTestC
     {
+        //Scoped for all calc tests
+        SalaryCalculator calculator = new SalaryCalculator();
+        
+        //DATA STRUCTURE that we use for all tests
+        // LIST, Dictionary, Array, Object, Tree, Stack, Database, Queue, Db Schema, Class, Struct
+
         /// <summary>
         /// Test the Hourly Salary
         /// </summary>
@@ -19,16 +26,30 @@ namespace SalCalcTest25
         public void AnnualSalaryTest()
         {
             //Arange - Setup the data
-            SalaryCalculator calculator = new SalaryCalculator();
-            decimal wage = 48.08m;
-            decimal exAnnualSalary = 100006.40m;
+            //decimal wage = 48.08m;
+            //decimal exAnnualSalary = 100006.40m;
 
             //Act - Access the data
-            decimal actAnnualSalary = calculator.GetAnnualSalary(wage);
+            decimal actAnnualSalary = calculator.GetAnnualSalary(48.08m);
 
             //Assert - Check the data
             //Assert.AreEqual(expected vs actual)
-            Assert.AreEqual(exAnnualSalary, actAnnualSalary);
+            Assert.AreEqual(100006.40m, actAnnualSalary);
+
+        }
+
+        [TestMethod]
+        public void HourlyWageTest()
+        {
+            //Arrange - Setup the data
+            decimal exWage = 48.08m;
+            decimal annualSalary = 100006.40m;
+
+            //Act - Access the data
+            decimal actWage = calculator.GetHourlyWage(annualSalary);
+
+            //Assert - Check the data   
+            Assert.AreEqual(exWage, actWage);
 
         }
     }
